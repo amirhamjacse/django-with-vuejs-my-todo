8598fb5b36f910474dcce5513a9884b74d2c7fe9
amirhamjacse <amirhamza824@gmail.com>
2024-10-23 11:43:07 +0600

    added new app

--- CONFIG/todo/models.py
+++ CONFIG/todo/models.py
@@ -1,3 +1,23 @@
+from typing import Any
 from django.db import models
 
-# Create your models here.
+
+class Task(models.Model):
+    title = models.CharField(
+        max_length=255, null=True, blank=False
+    )
+    description = models.TextField(
+        null=True, blank=True
+    )
+    is_completed = models.BooleanField(
+        default=False
+    )
+    due_date = models.DateField(
+        null=False, blank=False,
+    )
+    created_date = models.DateField(
+        auto_created=True
+    )
+
+    def __str__(self):
+        return self.title
